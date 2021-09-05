cd049611000e94473e0dd9f38fc2f1c30465b6b2
selinavarouqa <ceevarouqa@gmail.com>
2021-09-05 16:52:51 +0300

    working dynamic ansible inventory

--- terraform/development.tf
+++ terraform/development.tf
@@ -1,44 +1,40 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "dev-net"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "dev-net"
-  }
-}
-
-resource "aws_network_interface" "dev" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
-
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
-
 resource "aws_instance" "dev" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
 
   tags = {
-    Name = "docker-machine"
+    service = "docker"
   }
 
-  network_interface {
-    network_interface_id = aws_network_interface.dev.id
-    device_index         = 0
-  }
+  security_groups = [aws_security_group.allow-all.name]
 
   credit_specification {
     cpu_credits = "unlimited"
+  }
+}
+
+resource "aws_security_group" "allow-all" {
+  name        = "allow_all_rules"
+  description = "Allow inbound traffic from SSH and web"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-rules"
   }
 }
